20d6d998ba18e4e1608dfe311c2f43d78ab23308
ESK0060 <ESK0060@ESK-CPU-012.kastechssg.local>
2026-02-27 18:05:27 +0530

    added vercel url to cors

--- src/main/java/com/demo/demo/Configarations/SecurityConfig.java
+++ src/main/java/com/demo/demo/Configarations/SecurityConfig.java
@@ -77,7 +77,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource (){
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000","http://localhost:5173","https://your-frontend.vercel.app"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000","http://localhost:5173","https://movie-ui-jade.vercel.app"));
         configuration.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
